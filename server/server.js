afe84a8d011740e125c8bd5a654cfb1f02e5d0e7
alentajibaev <alentajibaev@gmail.com>
2025-03-29 04:53:45 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,14 +23,22 @@ app.post('/subscribe', (req, res) => {
 });
 
 app.get('/send', (req, res) => {
-  const payload = JSON.stringify({ title: 'Землетрясение!', body: 'Произошло землетрясение в вашем регионе!' });
-
-  subscriptions.forEach(sub => {
-    webpush.sendNotification(sub, payload).catch(err => console.error(err));
+  const payload = JSON.stringify({
+    title: '⚠️ Землетрясение!',
+    body: 'Укройтесь немедленно!'
   });
 
-  res.send('Уведомления отправлены');
+  for (let i = 0; i < 10; i++) {
+    setTimeout(() => {
+      subscriptions.forEach(sub => {
+        webpush.sendNotification(sub, payload).catch(err => console.error(err));
+      });
+    }, i * 300); // каждые 300 мс = 3 секунды на 10 уведомлений
+  }
+
+  res.send('10 уведомлений отправлено за 3 секунды!');
 });
+
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
